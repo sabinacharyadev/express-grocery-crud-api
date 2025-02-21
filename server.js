8efb6a8cbcd5292f27faf6c327d620768ea9262c
sabinbabu <sabinacharya444@gmail.com>
2025-02-21 13:34:53 +1100

    connection established between server and db engine

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { connectToMongoDB } from "./config/dbConfig.js";
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+
+connectToMongoDB();
 
 app.listen(PORT, (error) => {
   error
