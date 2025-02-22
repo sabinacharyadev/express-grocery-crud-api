9466891eb08691b2685d0b24d308802bf9171424
sabinbabu <sabinacharya444@gmail.com>
2025-02-22 22:21:43 +1100

    folder structure updated

---
+++ src/schema/employeesSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const employeesSchema = mongoose.Schema({
+  employee_name: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  join_date: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const employeesModel = mongoose.model("employee", employeesSchema);
+export default employeesModel;
