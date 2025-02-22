19f54a0f16a606f49ebfb9bd1db36f02ca36ec0a
sabinbabu <sabinacharya444@gmail.com>
2025-02-22 22:15:14 +1100

    customer routes added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectToMongoDB } from "./config/dbConfig.js";
 import employeesRouter from "./routes/employeesRouter.js";
+import customersRouter from "./routes/customersRouter.js";
 
 const PORT = 3000;
 const app = express();
@@ -11,6 +12,9 @@ connectToMongoDB();
 
 // Employees Routes
 app.use("/api/v1/employees", employeesRouter);
+
+// Customers Routes
+app.use("/api/v1/customers", customersRouter);
 
 app.listen(PORT, (error) => {
   error
